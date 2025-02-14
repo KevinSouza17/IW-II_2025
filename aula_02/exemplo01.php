3ea1b6e3dbc0889c94487648bb9f9e9be066e24f
Kevin Gabriel <161509299+KevinSouza17@users.noreply.github.com>
2025-02-14 16:59:33 -0300

    aula 2

---
+++ aula_02/exemplo01.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exemplo 01 - php</title>
+</head>
+<body>
+    <p>Oi eu sou o Kevin</p>
+    
+  <?php
+   echo "<p>Oi eu sou o Kevin</p>";
+   $nome = "Kevin";
+   echo "<p> Oi eu sou o $nome </p>";
+   echo "<p> Oi eu sou o ". $nome." </p>";
+   
+
+  ?>
+
+
+
+
+</body>
+</html>
